22b82d0aff6e0f429c0409c3ccd71dcfef2290c5
che-ri <rnjs1135@gmail.com>
2022-11-05 06:26:19 +0900

    [FEAT] FormFields => TFieldValues 제너릭명 변경 (#2)

--- components/common/rhfInput.tsx
+++ components/common/rhfInput.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { DeepMap, FieldError, Path, RegisterOptions, UseFormRegister } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 
-interface PropsTypes<FormFields> extends React.InputHTMLAttributes<HTMLInputElement> {
-    register: UseFormRegister<FormFields>;
+interface PropsTypes<TFieldValues> extends React.InputHTMLAttributes<HTMLInputElement> {
+    register: UseFormRegister<TFieldValues>;
     options?: RegisterOptions;
-    name: Path<FormFields>;
-    errors?: Partial<DeepMap<FormFields, FieldError>>;
+    name: Path<TFieldValues>;
+    errors?: Partial<DeepMap<TFieldValues, FieldError>>;
     helperClassName?: string; //helperText의 className
 }
 
 //기본적으로 input의 모든 속성들을 사용할 수 있습니다.
-//외부로부터 FormFields에 들어갈 타입을 정의해야합니다.
-//ex) type FormFields = { foo : string };
-export default function RHFInput<FormFields>({ register, options, name, errors = {}, helperClassName = '', ...props }: PropsTypes<FormFields>) {
+//외부로부터 TFieldValues에 들어갈 타입을 정의해야합니다.
+//ex) type TFieldValues = { foo : string };
+export default function RHFInput<TFieldValues>({ register, options, name, errors = {}, helperClassName = '', ...props }: PropsTypes<TFieldValues>) {
     return (
         <div>
             <input {...(register && register(name, options))} name={name} {...props} />
--- components/common/rhfSelect.tsx
+++ components/common/rhfSelect.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Path, UseFormRegister } from 'react-hook-form';
 
-interface PropsTypes<FormFields> extends React.SelectHTMLAttributes<HTMLSelectElement> {
-    register: UseFormRegister<FormFields>;
-    name: Path<FormFields>;
+interface PropsTypes<TFieldValues> extends React.SelectHTMLAttributes<HTMLSelectElement> {
+    register: UseFormRegister<TFieldValues>;
+    name: Path<TFieldValues>;
     helperClassName?: string; //helperText의 className
 }
 
 //children에 option들을 넣어주어야 합니다.
-export default function RHFSelect<FormFields>({ register, name, children, ...props }: PropsTypes<FormFields>) {
+export default function RHFSelect<TFieldValues>({ register, name, children, ...props }: PropsTypes<TFieldValues>) {
     return (
         <select {...register(name)} {...props}>
             {children}
